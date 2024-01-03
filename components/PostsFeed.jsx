af6b545e9950f222622576782b77352285ad751f
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2024-01-03 01:59:02 -0800

    Done with the Tweet Input component

--- components/PostsFeed.jsx
+++ components/PostsFeed.jsx
@@ -2,7 +2,7 @@ import TweetInput from "./TweetInput";
 
 export default function PostsFeed() {
   return (
-    <div className="sm:ml-20 xl:ml-96 flex-grow max-w-2xl border-gray-700 border-x">
+    <div className="sm:ml-20 xl:ml-96 flex-grow max-w-xl border-gray-700 border-x">
       <div className="px-3 py-2 text-lg sm:text-xl font-bold border-gray-700 border-b sticky top-0 z-50">
         Home
       </div>
--- components/TweetInput.jsx
+++ components/TweetInput.jsx
@@ -1,3 +1,4 @@
+import { CalendarIcon, ChartBarIcon, EmojiHappyIcon, LocationMarkerIcon, PhotographIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 
 export default function TweetInput() {
@@ -11,11 +12,37 @@ export default function TweetInput() {
           className="rounded-full object-cover"
         />
       </div>
-      <div>
-        <textarea
-          className="bg-transparent resize-none outline-none w-full min-h-[50px] text-lg"
-          placeholder="What's on your mind?"
-        />
+      <div className="w-full">
+        <div>
+          <textarea
+            className="bg-transparent resize-none outline-none w-full min-h-[50px] text-lg"
+            placeholder="What's on your mind?"
+          />
+        </div>
+
+        <div className="flex justify-between w-full border-gray-700 border-t pt-4">
+          <div className="flex">
+            {/* //Icons */}
+            <div className="iconAnimation">
+              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+            <div className="iconAnimation">
+              <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+            <div className="iconAnimation">
+              <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+            <div className="iconAnimation">
+              <CalendarIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+            <div className="iconAnimation">
+              <LocationMarkerIcon className="h-[22px] text-[#1d9bf0]" />
+            </div>
+          </div>
+          <div>
+            <button className="bg-[#1d9bf0] px-4 py-1.5 rounded-full">Post</button>
+          </div>
+        </div>
       </div>
     </div>
   );
