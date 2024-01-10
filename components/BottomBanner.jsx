528e0b6f56432c547c4a68839af224c833d11f98
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2024-01-09 23:44:47 -0800

    style: fixed the styling for bottom banner for smaller screens

--- components/BottomBanner.jsx
+++ components/BottomBanner.jsx
@@ -3,7 +3,7 @@ import SignupModal from "./modals/SignupModal";
 
 export default function BottomBanner() {
   return (
-    <div className="flex justify-center items-center py-3 space-x-[200px] fixed bottom-0 bg-[#1d9bf0] w-full h-[80px]">
+    <div className="flex justify-center items-center py-3 md:space-x-[200px] fixed bottom-0 bg-[#1d9bf0] w-full h-[80px]">
       
       <div className="text-white hidden xl:flex flex-col">
         <h1 className="font-bold text-2xl">Don't miss what's happening</h1>
